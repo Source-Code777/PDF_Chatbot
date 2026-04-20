2100ac1f4bada580214f324c1f4d1ac2376fa7fb
Aasim Athar Alam <159575330+Source-Code777@users.noreply.github.com>
2026-04-20 21:46:45 +0530

    Added a basic list memory to the chatbot it keeps track of last the chats between user and assistant

--- src/llm.py
+++ src/llm.py
@@ -5,30 +5,58 @@ def get_llm():
     generator = pipeline(
         task="text-generation",
         model="HuggingFaceTB/SmolLM2-1.7B",
-        max_new_tokens=100,
+        max_new_tokens=200,
         do_sample=False
     )
     return generator
 
+def format_chat_history(chat_history):
+    history_text=""
+    for rol,msg in enumerate(chat_history):
+        if rol=="user":
+            history_text+=f"User: {msg}\n"
+        else:
+            history_text+=f"Assistant: {msg}\n"
+        return history_text
 
-def generate_answer(llm, query, context):
+
+
+
+def generate_answer(llm, query, context, chat_history):
+    history_text=format_chat_history(chat_history)
     prompt_template = PromptTemplate.from_template("""
-Answer the question based on the context below.
-If you don't know the answer, just say you don't know.
+    You are a helpful AI assistant.
 
-Context:
-{context}
-
-Question:
-{query}
-
-Answer:
-""")
-
+    Your job is to READ the context and EXPLAIN it in a simple and clear way.
+    
+    Rules:
+    - Do NOT copy text directly from the context
+    - Always explain in your own words
+    - Keep answers short and easy to understand
+    - If the question depends on previous conversation, use the history
+    
+    Conversation History:
+    {history}
+    
+    Context:
+    {context}
+    
+    Question:
+    {query}
+    
+    Answer (simple explanation):
+    """
+    )
     final_prompt = prompt_template.format(
         context=context,
-        query=query
+        query=query,
+        history=history_text
     )
 
     response = llm(final_prompt)
-    return response[0]['generated_text']
+    full_text=response[0]['generated_text']
+    if final_prompt in full_text:
+        answer = full_text.split(final_prompt)[-1].strip()
+    else:
+        answer = full_text.strip()
+    return answer
--- src/main.py
+++ src/main.py
@@ -7,20 +7,20 @@ if __name__ == "__main__":
     path=r"C:\Users\aasim\OneDrive\Desktop\Notes\NLP\cs224n_winter2023_lecture1_notes_draft.pdf"
     docs = load_pdf(path)
     chunks = split_documents(docs)
-
     vectorstore = create_vectorstore(chunks)
-
-    query = "What is Word2Vec?"
-
-    results = vectorstore.similarity_search(query, k=3)
-
-
-    context = "\n\n".join([doc.page_content for doc in results])
-
-
     llm = get_llm()
+    chat_history=[]
 
-    answer = generate_answer(llm, query, context)
+    while True:
+        query=input("\nAsk: ")
+        if query.lower() in ["exit","quit"]:
+            break
+        results=vectorstore.similarity_search(query,k=3)
+        context="\n\n".join([doc.page_content for doc in results])
+        answer=generate_answer(llm,context,query,chat_history)
+        print("\n---Final Answer---\n")
+        print(answer)
+        chat_history.append(("user",query))
+        chat_history.append(("Assistant",answer))
+        chat_history=chat_history[-6:]
 
-    print("\n--- FINAL ANSWER ---\n")
-    print(answer)
